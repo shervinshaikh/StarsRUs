854b50cc6b7ccc390bc6d67515ef4a655497894a
Shervin Shaikh <shervinater@gmail.com>
2013-03-03 15:51:44 -0800

    new gui stuff

---
+++ src/com/shervinshaikh/starsrus/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.shervinshaikh.starsrus;
+
+import static spark.Spark.*;
+
+import spark.*;
+
+public class HelloWorld {
+
+   public static void main(String[] args) {
+      
+      get(new Route("/hello") {
+         @Override
+         public Object handle(Request request, Response response) {
+            return "Hello World!";
+         }
+      });
+
+   }
+
+}
